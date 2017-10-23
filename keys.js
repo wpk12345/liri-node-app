3df7b8386821c4cf47e3a7d49609cfe2a0049fba
William King <wpk12345@gmail.com>
2017-10-23 01:47:11 -0400

    Another Commit

---
+++ keys.js
@@ -0,0 +1,10 @@
+console.log('this is loaded');
+
+var twitterKeys = {
+  consumer_key: 'Wu8mUzdfmVWgRYVxbG6pJLOAl',
+  consumer_secret: 'EpFYjXETydYHk2dz5Fhpy2MrZiAqgTw8iOtSLkK1TvMZ4EzXnk',
+  access_token_key: '920774463301570561-1yq8GA4cqIImYqPtCrVgMuOHrqHdV5d',
+  access_token_secret: 'UQajIlTUdG6pcTGcP6UMs0AMwqWEpBvaNvKSNabFEYpE7',
+}
+
+module.exports = twitterKeys;
--- liri.js
+++ liri.js
@@ -1,1 +1,3 @@
 var request = require('request');
+
+var twitterKeys = require("./keys.js");
