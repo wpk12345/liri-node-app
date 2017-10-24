59919d376601e3f20dc21aa7d2d8eb0d3daea30f
William King <wpk12345@gmail.com>
2017-10-23 20:19:18 -0400

    commit with twitter done

--- keys.js
+++ keys.js
@@ -1,10 +1,11 @@
-console.log('this is loaded');
+
 
 var twitterKeys = {
-  consumer_key: 'Wu8mUzdfmVWgRYVxbG6pJLOAl',
-  consumer_secret: 'EpFYjXETydYHk2dz5Fhpy2MrZiAqgTw8iOtSLkK1TvMZ4EzXnk',
-  access_token_key: '920774463301570561-1yq8GA4cqIImYqPtCrVgMuOHrqHdV5d',
-  access_token_secret: 'UQajIlTUdG6pcTGcP6UMs0AMwqWEpBvaNvKSNabFEYpE7',
+  consumer_key: 'ONMgEP5cbEwdnYOiw1I8nJLIS',
+  consumer_secret: 'GOewZpKgHJoFKUFHcfIuzGdmyxGSAH7wVYcAn5vJS4FtoXJYXk',
+  access_token: '920774463301570561-JU0g8CLfFlAfR0M6y9Ui6yGM5obSUe9',
+  access_token_secret: 'G2aIO67CqtgvEqnncdzpfZQ9Rlape9Y7GJOzX7vyXhOH6',
+  timeout_ms:           60*1000,  // optional HTTP request timeout to apply to all requests. 
 }
 
 module.exports = twitterKeys;
--- liri.js
+++ liri.js
@@ -1,3 +1,51 @@
+//Require variables of files and packages
+console.log('the bot is starting');
 var request = require('request');
-
+var fs = require("fs");
+var Twit = require('twit');
 var twitterKeys = require("./keys.js");
+var T = new Twit(twitterKeys);
+var action = process.argv[2];
+var params = {
+    count: 20,
+    screen_name: 'wpk543210'
+  };
+
+switch (action) {
+  case "my-tweets":
+    tweet();
+    break;
+
+  case "spotify-this-song":
+    spotify();
+    break;
+//see instructions for process.argv[3]
+  case "movie-this":
+    movie();
+    break;
+
+  case "do-what-it-says":
+    doWhat();
+    break;
+}
+//============================================Twitter Function====================================================================
+
+function tweet(){
+    
+    T.get('statuses/user_timeline', params , function(err, data) {
+      for (var i = 0; i < data.length ; i++) {
+        console.log(data[i].text);
+      }
+  })
+    };
+//============================================Spotify Function====================================================================
+
+    function spotify() {}
+    
+
+
+
+
+  
+
+
