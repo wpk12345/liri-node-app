749812ebe93cb837e7f6a8e6736e17ff256d4809
William King <wpk12345@gmail.com>
2017-10-25 18:04:36 -0400

    Finished twitter and spotify api

--- liri.js
+++ liri.js
@@ -1,10 +1,10 @@
 //'Require' variables of files and packages
-console.log('the bot is starting');
 var request = require('request');
 var fs = require("fs");
 var twitterKeys = require("./keys.js");
 var Twit = require('twit');
 var Spotify = require('node-spotify-api');
+//playing around with constructor functions
 var T = new Twit(twitterKeys);
 //assigning argv[2] to the function action for our switch/case method
 var action = process.argv[2];
@@ -45,19 +45,44 @@ function tweet(){
 //============================================Spotify Function====================================================================
 
     function spotify() {
-        var Spotify = require('node-spotify-api');
+        // var Spotify = require('node-spotify-api');
         
        var spotify = new Spotify({
          id: '102d288e13734766b30efb0caf0c7eb9',
          secret: 'c15786f446024e2a9c8e606ba002781b'
        });
+   //need to do a for loop for process.argv if name has more than one word
+   // Grab all of the command line arguments from Node.
+       var nodeArg = process.argv;
+       var queryArray = [];
+   // for loop that pushes all the process.argv's into an array for the query
+       for (var i = 3; i < nodeArg.length; i++) {
         
-       spotify.search({ type: 'track', query: process.argv[3] }, function(err, data) {
+          // We then "push" (add) each of these strings to our queryArray.
+          queryArray.push(nodeArg[i]);      
+        }
+       spotify.search({ type: 'track', query: queryArray, limit: 1 }, function(err, data) {
          if (err) {
-           return console.log('Error occurred: ' + err);
+          spotify
+          .request('https://api.spotify.com/v1/tracks/0hrBpAOgrt8RXigk83LLNE')
+          .then(function(data) {
+          console.log('Artist: ' + data.album.artists[0].name); 
+          console.log('Song Name: ' + data.name); 
+          console.log("Preview Link of Song: " + data.preview_url); 
+          console.log("Album Name: " + data.album.name); 
+
+           
+          })
+          .catch(function(err) {
+            console.error('Error occurred: ' + err); 
+          });
          }
+        //returning the 'artist', 'name of song', 'a preview link' and 'album name'
+        console.log('Artist: ' + data.tracks.items[0].album.artists[0].name); 
+        console.log('Song Name: ' + data.tracks.items[0].name); 
+        console.log("Preview Link of Song: " + data.tracks.items[0].preview_url);
+        console.log("Album Name: " + data.tracks.items[0].album.name);
         
-       console.log(data); 
        })
 
     };
